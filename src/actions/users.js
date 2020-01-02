7e47738fd2b08eaf94019a272c63be4ddc451c9f
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-01-03 01:30:03 +0800

    FEATURE: add a new question. Todo: need to add the question to the user as well

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,10 @@
-export const RECEIVE_USERS = "RECEIVE_USER"
+export const RECEIVE_USERS = "RECEIVE_USER";
 
-export function receiveUsers(users) { 
-    return { 
-        type: RECEIVE_USERS, 
-        users
-    }
+export function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users
+  };
 }
+
+//TODO: add the question to the USER depending on who is logged in
