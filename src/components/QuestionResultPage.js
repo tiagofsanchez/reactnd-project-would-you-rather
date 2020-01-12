61d75ec57c996d34320c329538b4a406467ce166
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-01-12 21:20:24 +0800

    CHORE: typos

---
+++ src/components/QuestionResultPage.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Progress } from "semantic-ui-react";
+
+import NavBar from "./NavBar";
+
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const CardContainer = styled.div`
+  margin: auto;
+  width: 70%;
+  border: solid #e03997 1px;
+  padding: 10px 20px 10px 20px;
+  border-radius: 4px;
+  text-align: center;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Flex = styled.div`
+  display: flex;
+`;
+
+//TODO: check why is avatar different from the Leaderboard
+const Avatar = styled.img`
+  height: 80px;
+`;
+
+const Option = styled.img`
+  height: 20px;
+  margin: 10px;
+`;
+
+const optionOne = "https://image.flaticon.com/icons/svg/752/752665.svg";
+const optionTwo = "https://image.flaticon.com/icons/svg/752/752665.svg";
+
+class QuestionResultsPage extends Component {
+  render() {
+    const { avatarURL, name } = this.props;
+    console.log(this.props);
+    return (
+      <div>
+        <NavBar />
+        <CardContainer>
+          <FlexColumn>
+            <Avatar src={avatarURL} alt={name} />
+            <h3>{name}</h3>
+          </FlexColumn>
+          <FlexColumn>
+            <Flex>
+              <Option src={optionTwo} />
+              <Progress />
+            </Flex>
+            <Flex>
+              <Option src={optionTwo} />
+              <Progress />
+            </Flex>
+          </FlexColumn>
+        </CardContainer>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users, questions, authUser }, props) {
+  const id = props.match.params.id;
+  const name = users[questions[id].author].name;
+  const avatarURL = users[questions[id].author].avatarURL;
+  const optionOneVotes = questions[id].optionOne.votes.length;
+  const optionTwoVotes = questions[id].optionTwo.votes.length;
+  const totalVotes = optionOneVotes + optionTwoVotes;
+
+  return {
+    name,
+    avatarURL,
+    optionOneVotes,
+    optionTwoVotes,
+    totalVotes
+  };
+}
+
+export default connect(mapStateToProps)(QuestionResultsPage);
